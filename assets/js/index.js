46a033da3a959febf802f79febc564bcb4764aba
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-05-11 22:27:34 +0300

    Add news example

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,76 +1,19 @@
 "use strict";
 
-const addNewPostBtn = document.querySelector(".addNewPostButton");
 const postsWrapper = document.querySelector(".postsWrapper");
-const searchForm = document.querySelector(".searchForm");
-const addNewPostForm = document.querySelector(".newPostForm");
-postsWrapper.append(...posts.map((post) => createNewPost(post)));
 
-addNewPostForm.addEventListener("submit", createPostSubmitHandler);
 
-addNewPostBtn.addEventListener("click", ({currentTarget}) => {
-  addNewPostForm.parentNode.hidden = false;
-  currentTarget.hidden = true;
-})
+renderPosts(postsWrapper, posts);
 
-function createPostSubmitHandler(e) {
-  e.preventDefault();
-  const {
-    target,
-    target: {
-      parentNode,
-      elements: {
-        newPostTitle: { value: newPostTitle },
-        newPostContent: { value: newPostContent },
-      },
-    },
-  } = e;
-  const postObj = {
-    title: newPostTitle,
-    content: newPostContent,
-    creationDate: new Date(),
-  };
-
-  posts.push(postObj);
-
-  postsWrapper.append(createNewPost(postObj));
-  target.reset();
-  parentNode.hidden = true;
-  addNewPostBtn.hidden = false;
+function renderPosts(container, posts) {
+  clearChildren(container);
+  container.append(...posts.map((post) => createNewPost(post)));
 }
 
-function createPostResetHandler(e) {}
-
-function createNewPost({ title, content, creationDate }) {
-  return createElement(
-    "article",
-    { classNames: ["post"] },
-    createPostHeader(title, creationDate),
-    createPostContent(content)
-  );
+function clearChildren(container) {
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
 }
 
-function createPostHeader(title, creationDate) {
-  return createElement(
-    "header",
-    { classNames: ["postHeader"] },
-    createElement(
-      "h2",
-      { classNames: ["postTitle"] },
-      document.createTextNode(title)
-    ),
-    createElement(
-      "div",
-      { classNames: ["postCreationDate"] },
-      document.createTextNode(creationDate.toLocaleDateString())
-    )
-  );
-}
 
-function createPostContent(content) {
-  return createElement(
-    "p",
-    { classNames: ["postContent"] },
-    document.createTextNode(content)
-  );
-}
